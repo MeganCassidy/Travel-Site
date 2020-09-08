5b790c89c9cee26055ccba37d287624886a8d177
Megan Cassidy <megan_cassidy123@hotmail.co.uk>
2020-09-08 21:29:01 +0100

    database set up

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
+let Post = require('./models/posts').Post;
+
+mongoose.connect('mongodb://localhost/travels', { useNewUrlParser: true });
+
+let post1 = new Post({
+    id: 2,
+    title: 'Statue of liberty',
+    date: new Date(),
+    description: 'Some description',
+    text: 'Some text',
+    country: 'USA',
+    imageURL: '/images/1.jpg'
+});
+
+post1.save();
 
 app.use(express.static('public'));
 
